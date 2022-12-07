2b304cff2998a1dfd77d23c7a977359ba33e8455
Boris Demin <demin.b.d@gmail.com>
2022-12-07 22:37:43 +0300

    add MakeFile includs basic commands(run-dist etc.) and add Picocli lib to project in clacc App.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff",
+        description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true)
+public class App implements Runnable {
+
+    @Override
+    public void run() {
     }
+    public static void main(String[] args) {
+
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+
 }
+
