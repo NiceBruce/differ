6a3c73e042534a5fa2e74fa33d64beb98d4442b0
Boris Demin <demin.b.d@gmail.com>
2022-12-29 22:40:22 +0300

    fix hexlet check

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -26,17 +26,20 @@ import java.util.concurrent.Callable;
     private String filePath2;
 
     @Override
-    public Integer call() {
+    public Integer call() throws Exception {
 
-        try {
-            String formattedDiff = Differ.generate(filePath1, filePath2, format);
-            System.out.println(formattedDiff);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            return ERROR_EXIT_CODE;
-        }
+        String formattedDiff = Differ.generate(filePath1, filePath2, format);
+        System.out.println(formattedDiff);
+//        try {
+//            String formattedDiff = Differ.generate(filePath1, filePath2, format);
+//            System.out.println(formattedDiff);
+//        } catch (Exception e) {
+//            System.err.println(e.getMessage());
+//            return ERROR_EXIT_CODE;
+//        }
 
-        return SUCCESS_EXIT_CODE;
+//        return SUCCESS_EXIT_CODE;
+        return 0;
     }
     public static void main(String[] args) throws Exception {
         int exitCode = new CommandLine(new App()).execute(args);
