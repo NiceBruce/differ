cfccb058af7ec11ab064e403cf3b1a6dbc92cbea
Boris Demin <demin.b.d@gmail.com>
2022-12-31 12:08:08 +0300

    work on edits v3.0

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,50 +1,22 @@
 package hexlet.code;
 
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.junit.jupiter.api.BeforeAll;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.skyscreamer.jsonassert.JSONAssert;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
-import java.util.stream.Stream;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 public class DifferTest {
-    private static final int STYLISH_CASE_JSON12 = 0;
-    private static final int STYLISH_CASE_JSON34 = 1;
-    private static final int PLAIN_CASE_JSON12 = 2;
-    private static final int PLAIN_CASE_JSON34 = 3;
-    private static final int EMPTY_CASE = 4;
-    private static final int FILE1_EMPTY_CASE = 5;
-    private static final int FILE2_EMPTY_CASE = 6;
-
-
-
-    private static String dataFromJsonFile1;
-    private static String dataFromJsonFile2;
-    private static String dataFromYmlFile1;
-    private static String dataFromYmlFile2;
-    private static String dataFromJsonFile3;
-    private static String dataFromJsonFile4;
-    private static String dataFromYmlFile3;
-    private static String dataFromYmlFile4;
-
-    private static String[] expectedDifferResultArray;
-    private static String resultDiffBeetweenJsonCase1;
-    private static String resultDiffBeetweenJsonCase2;
+    private static String resultJson;
+    private static String resultPlain;
+    private static String resultStylish;
     private static String emptyfile;
-
 
 
     public static Path getPathFile(String fileName) {
@@ -52,96 +24,39 @@ public class DifferTest {
                 .toAbsolutePath().normalize();
     }
 
-
-    public static String getDataFromFile(String fileName) throws Exception {
-
+    public static String readFixture(String fileName) throws Exception {
         Path filePath = getPathFile(fileName);
-
-        if (!Files.exists(filePath)) {
-            throw new Exception("File '" + filePath + "' does not exist");
-        }
         return Files.readString(filePath);
     }
 
 
-
-    public static <T> Map<T, T> convertDataToMap(String fileName, String fileFormat) throws Exception {
-        String dataFromFile = getDataFromFile(fileName);
-        ObjectMapper dataMapper = (fileFormat.equals("yml"))
-                ? new ObjectMapper(new YAMLFactory()) : new ObjectMapper();
-        TypeReference<Map<T, T>> typeReference = new TypeReference<>() {
-        };
-
-        return dataMapper.readValue(dataFromFile, typeReference);
-    }
-
-    public static String[] convertDataToArrayStr(String fileName) throws Exception {
-        String dataFromFile = getDataFromFile(fileName);
-        ObjectMapper dataMapper = new ObjectMapper();
-        TypeReference<String[]> typeReference = new TypeReference<>() {
-        };
-
-        String[] res = dataMapper.readValue(dataFromFile, typeReference);
-
-        return res;
-    }
-
     @BeforeAll
     public static void beforeAll() throws Exception {
-        dataFromJsonFile1 = getPathFile("file1.json").toString();
-        dataFromJsonFile2 = getPathFile("file2.json").toString();
-        dataFromJsonFile3 = getPathFile("file3.json").toString();
-        dataFromJsonFile4 = getPathFile("file4.json").toString();
-        dataFromYmlFile1 = getPathFile("file1.yml").toString();
-        dataFromYmlFile2 = getPathFile("file2.yml").toString();
-        dataFromYmlFile3 = getPathFile("file3.yml").toString();
-        dataFromYmlFile4 = getPathFile("file4.yml").toString();
+        resultJson = readFixture("result_json.json");
+        resultPlain = readFixture("result_plain.txt");
+        resultStylish = readFixture("result_stylish.txt");
         emptyfile = getPathFile("empty.json").toString();
-        expectedDifferResultArray = convertDataToArrayStr("resultArrayOfStrings.json");
-        resultDiffBeetweenJsonCase1 = getDataFromFile("resultDiffBeetweenJson1_2.json");
-        resultDiffBeetweenJsonCase2 = getDataFromFile("resultDiffBeetweenJson3_4.json");
+
     }
 
-    private static Stream<Arguments> provideStringsForIsBlank() {
-        return Stream.of(
-                Arguments.of(resultDiffBeetweenJsonCase1,
-                        dataFromJsonFile1, dataFromJsonFile2, "json"),
-                Arguments.of(resultDiffBeetweenJsonCase2,
-                        dataFromJsonFile3, dataFromJsonFile4, "json"),
-                Arguments.of(expectedDifferResultArray[STYLISH_CASE_JSON12],
-                        dataFromJsonFile1, dataFromJsonFile2, "stylish"),
-                Arguments.of(expectedDifferResultArray[STYLISH_CASE_JSON34],
-                        dataFromJsonFile3, dataFromJsonFile4, "stylish"),
-                Arguments.of(expectedDifferResultArray[PLAIN_CASE_JSON12],
-                        dataFromJsonFile1, dataFromJsonFile2, "plain"),
-                Arguments.of(expectedDifferResultArray[PLAIN_CASE_JSON34],
-                        dataFromJsonFile3, dataFromJsonFile4, "plain"),
-                Arguments.of(resultDiffBeetweenJsonCase1,
-                        dataFromYmlFile1, dataFromYmlFile2, "json"),
-                Arguments.of(resultDiffBeetweenJsonCase2,
-                        dataFromYmlFile3, dataFromYmlFile4, "json"),
-                Arguments.of(expectedDifferResultArray[STYLISH_CASE_JSON12],
-                        dataFromYmlFile1, dataFromYmlFile2, "stylish"),
-                Arguments.of(expectedDifferResultArray[STYLISH_CASE_JSON34],
-                        dataFromYmlFile3, dataFromYmlFile4, "stylish"),
-                Arguments.of(expectedDifferResultArray[PLAIN_CASE_JSON12],
-                        dataFromYmlFile1, dataFromYmlFile2, "plain"),
-                Arguments.of(expectedDifferResultArray[PLAIN_CASE_JSON34],
-                        dataFromYmlFile3, dataFromYmlFile4, "plain")
-//                Arguments.of(expectedDifferResultArray[EMPTY_CASE],
-//                        emptyfile, emptyfile, "stylish")
-//                Arguments.of(expectedDifferResultArray[FILE1_EMPTY_CASE],
-//                        dataFromJsonFile1, emptyfile, "stylish"),
-//                Arguments.of(expectedDifferResultArray[FILE2_EMPTY_CASE],
-//                        emptyfile, dataFromJsonFile2, "stylish")
-
-        );
-    }
     @ParameterizedTest
-    @MethodSource("provideStringsForIsBlank")
-    public final void differTest(String expeted, String filepath1, String filepath2,
-                        String format) throws Exception {
-        assertEquals(expeted, Differ.generate(filepath1, filepath2, format));
+    @ValueSource(strings = {"json", "yml"})
+    public final void generateTest(String format) throws Exception {
+        String filePath1 = getPathFile("file1." + format).toString();
+        String filePath2 = getPathFile("file2." + format).toString();
+
+        assertThat(Differ.generate(filePath1, filePath2))
+                .isEqualTo(resultStylish);
+
+        assertThat(Differ.generate(filePath1, filePath2, "stylish"))
+                .isEqualTo(resultStylish);
+
+        assertThat(Differ.generate(filePath1, filePath2, "plain"))
+                .isEqualTo(resultPlain);
+
+        String actualJson = Differ.generate(filePath1, filePath2, "json");
+        JSONAssert.assertEquals(resultJson, actualJson, false);
+
     }
 
     @Test
@@ -154,6 +69,7 @@ public class DifferTest {
 
     @Test
     void testParserThrowsExceptionWhenEmptyFile() {
+
         Throwable exception = assertThrows(Exception.class,
                 () -> Differ.generate(emptyfile, emptyfile));
         assertTrue(exception.getMessage().contains("No content to map due to end-of-input"));
